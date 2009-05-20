2ff3530b00aa025e6ef7c2529e4a3130e14bc377
A <sidorenko@gmail.com>
2009-05-20 14:41:55 +0300

    a

--- emacs/rc/emacs-flymake.el
+++ emacs/rc/emacs-flymake.el
@@ -15,26 +15,27 @@
 
 (add-hook 'find-file-hook 'flymake-find-file-hook)
 
-;; do not use flymake for c, c++ and c#
-
 (setq flymake-allowed-file-name-masks 
-      (delete '("\\.c\\'" flymake-simple-make-init) 
-              flymake-allowed-file-name-masks))
-(setq flymake-allowed-file-name-masks 
-      (delete '("\\.cc\\'" flymake-simple-make-init) 
-              flymake-allowed-file-name-masks))
-(setq flymake-allowed-file-name-masks 
-      (delete '("\\.cpp\\'" flymake-simple-make-init) 
-              flymake-allowed-file-name-masks))
-(setq flymake-allowed-file-name-masks 
-      (delete '("\\.h\\'" flymake-simple-make-init) 
-              flymake-allowed-file-name-masks))
-(setq flymake-allowed-file-name-masks 
-      (delete '("\\.hpp\\'" flymake-simple-make-init) 
-              flymake-allowed-file-name-masks))
-(setq flymake-allowed-file-name-masks 
-      (delete '("\\.cs\\'" flymake-simple-make-init) 
-              flymake-allowed-file-name-masks))
+  '(
+    ("\\.xml\\'" flymake-xml-init)
+    ("\\.html?\\'" flymake-xml-init)
+    ("\\.p[ml]\\'" flymake-perl-init)
+    ("\\.php[345]?\\'" flymake-php-init)
+    ;;("\\.h\\'" flymake-master-make-header-init flymake-master-cleanup)
+    ;;("\\.java\\'" flymake-simple-make-java-init flymake-simple-java-cleanup)
+    ("[0-9]+\\.tex\\'" flymake-master-tex-init flymake-master-cleanup)
+    ("\\.tex\\'" flymake-simple-tex-init)
+    ;;("\\.idl\\'" flymake-simple-make-init)
+    ;; ("\\.cpp\\'" 1)
+    ;; ("\\.java\\'" 3)
+    ;; ("\\.h\\'" 2 ("\\.cpp\\'" "\\.c\\'")
+    ;; ("[ \t]*#[ \t]*include[ \t]*\"\\([\w0-9/\\_\.]*[/\\]*\\)\\(%s\\)\"" 1 2))
+    ;; ("\\.idl\\'" 1)
+    ;; ("\\.odl\\'" 1)
+    ;; ("[0-9]+\\.tex\\'" 2 ("\\.tex\\'")
+    ;; ("[ \t]*\\input[ \t]*{\\(.*\\)\\(%s\\)}" 1 2 ))
+    ;; ("\\.tex\\'" 1)
+    ))
 
 ;; ---
 
