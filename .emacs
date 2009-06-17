c55971cd0d178d14f9d7c8252b64eb199db6abe1
Andrey <sidorenko@gmail.com>
2009-06-18 00:10:33 +0300

    a

--- .emacs
+++ .emacs
@@ -17,6 +17,7 @@
 (load "~/emacs/rc/emacs-recentf.el")
 (load "~/emacs/rc/emacs-shell.el")
 (load "~/emacs/rc/emacs-ocaml.el")
+(load "~/emacs/rc/emacs-scala.el")
 
 (load "~/emacs/rc/emacs-file-editing.el")
 
---
+++ emacs/rc/emacs-scala.el
@@ -0,0 +1,27 @@
+(add-to-list 'load-path "~/emacs/scala-mode")
+
+(require 'scala-mode-auto)
+
+(add-to-list 'load-path "~/emacs/yasnippet-read-only")
+
+
+;; does not work FIXME: to check why
+;;(require 'yasnippet-bundle)
+
+(require 'yasnippet)
+
+(yas/initialize)
+
+(setq yas/my-directory "~/emacs/yasnippet-read-only/snippets")
+(yas/load-directory yas/my-directory)
+
+;(add-hook 'scala-mode-hook
+;            '(lambda ()
+;               (yas/minor-mode-on)
+;               ))
+
+;;(add-hook 'scala-mode-hook
+;;          '(lambda ()
+;;             (yas/minor-mode-on)
+
+;; ---
