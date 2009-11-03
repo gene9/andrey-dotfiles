b5ec8c417483c2ae1b1c8791a9006f824d9c63ed
A <sidorenko@gmail.com>
2009-11-03 13:40:29 +0200

    a

--- emacs/rc/emacs-daemon.el
+++ emacs/rc/emacs-daemon.el
@@ -8,8 +8,10 @@
 ;;  (server-edit)
   (make-frame-invisible nil t))                                                                     
 
-(if (memq window-system '(win32 w32))
-    (global-set-key (kbd "C-x C-c") 'my-done)
-)
+;; disabled at this moment
+
+;;(if (memq window-system '(win32 w32))
+;;    (global-set-key (kbd "C-x C-c") 'my-done)
+;;)
 
 ;; ---
