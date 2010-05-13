8ccb04fd8ecfe095b18ff0982865443731538bc9
gene9 <a@example.com>
2010-05-13 23:14:48 +0300

    a

--- emacs/rc/emacs-fonts.el
+++ emacs/rc/emacs-fonts.el
@@ -1,6 +1,8 @@
 
 ;; Change font to lucida console and setup initial window size
-(add-to-list 'default-frame-alist '(font . "-*-Lucida Console-normal-r-*-*-12-108-*-*-c-*-*-ansi-"))
+(if (eq system-type 'windows-nt)    
+	(add-to-list 'default-frame-alist '(font . "-*-Lucida Console-normal-r-*-*-12-108-*-*-c-*-*-ansi-")))
+
 (add-to-list 'default-frame-alist '(height . 60))
 (add-to-list 'default-frame-alist '(width . 80))
 ;;(add-to-list 'default-frame-alist '(background-color . "grey92"))
